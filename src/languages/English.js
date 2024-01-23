ef1e17468dfad06cca97ae54bba0fff5befb98a6
Kamel Bendimerad <kamel.bendimerad@gmail.com>
2024-01-23 03:08:18 +0100

    english dictionnary set up

---
+++ src/languages/English.js
@@ -0,0 +1,11 @@
+const english = {
+    email: 'email',
+    password: 'password',
+    signIn: 'Sign In',
+    signUp: 'Sign Up',
+    repeatPassword: 'Repeat password',
+    firstName: 'First name',
+    lastName: 'Last name',
+  };
+  
+  export default english;
